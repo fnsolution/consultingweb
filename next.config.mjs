f4db5f5508812e7ff6bbfda37fe3aa11f595aba9
fnsolution <fnsolution007@gmail.com>
2026-04-15 16:42:51 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/무상진단",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/개인재무설계",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/부동산관리",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/인사제도",
+        destination: "/contact",
+        permanent: true,
+      },
+      {
+        source: "/정부지원사업",
+        destination: "/contact",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
